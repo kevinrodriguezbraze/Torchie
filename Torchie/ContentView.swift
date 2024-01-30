2b1d465580e79328e78288fa0f7d1cfb2513073e
kevinrodriguezbraze <kevin.rodriguez@braze.com>
2024-01-30 09:51:28 -0500

    Initial Commit

---
+++ Torchie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Torchie
+//
+//  Created by Kevin Rodriguez on 1/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Torchie/TorchieApp.swift
@@ -0,0 +1,17 @@
+//
+//  TorchieApp.swift
+//  Torchie
+//
+//  Created by Kevin Rodriguez on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TorchieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
