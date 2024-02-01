ae9bc926ef31cb7cfa53049ead06bb36240d3551
kevinrodriguezbraze <kevin.rodriguez@braze.com>
2024-01-31 20:18:29 -0500

    Initial commit

--- Torchie/ContentView.swift
+++ Torchie/ContentView.swift
@@ -6,17 +6,192 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+
+    //VARIABLES
+    
+    @State private var externalID = ""
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var phoneNumber = ""
+    @State private var date = Date.now
+    
+    @State private var customAttributeKey = ""
+    @State private var customAttributeValue = ""
+    
+    @State private var customEventName = ""
+    @State private var customEventPropKey = ""
+    @State private var customEventPropValue = ""
+    
+    @State private var productID = ""
+    @State private var currency = "USD"
+    @State private var price = 0.0
+    @State private var quantity = 1
+    @State private var purchaseEventPropKey = ""
+    @State private var purchaseEventPropValue = ""
+    
+    @State private var showAlert = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Change User") {
+                    HStack {
+                        TextField("External ID", text: $externalID)
+                        
+                        Button("Set", action: {
+                            AppDelegate.braze?.changeUser(userId: externalID)
+                            UINotificationFeedbackGenerator().notificationOccurred(.success)
+                            externalID = ""
+                        })
+                        
+                    }
+                    
+                }
+                
+                Section("Main Attributes") {
+                    HStack {
+                        TextField("First Name", text: $firstName)
+                        
+                        Button("Set", action: {
+                            AppDelegate.braze?.user.set(firstName: firstName)
+                            UINotificationFeedbackGenerator().notificationOccurred(.success)
+                            firstName = ""
+                        })
+                    }
+                    
+                    HStack {
+                        TextField("Last Name", text: $lastName)
+                        
+                        Button("Set", action: {
+                            AppDelegate.braze?.user.set(lastName: lastName)
+                            UINotificationFeedbackGenerator().notificationOccurred(.success)
+                            lastName = ""
+                        })
+                    }
+                    
+                    
+                    
+                    HStack {
+                        TextField("Email Address", text: $email)
+                        
+                        Button("Set", action: {
+                            AppDelegate.braze?.user.set(email: email)
+                            UINotificationFeedbackGenerator().notificationOccurred(.success)
+                            email = ""
+                        })
+                    }
+                    
+                    
+                    HStack {
+                        TextField("Phone Number", text: $phoneNumber)
+                        
+                        Button("Set", action: {
+                            AppDelegate.braze?.user.set(phoneNumber: phoneNumber)
+                            UINotificationFeedbackGenerator().notificationOccurred(.success)
+                            phoneNumber = ""
+                        })
+                    }
+                    
+                    
+                    HStack {
+                        DatePicker("Date of Birth", selection: $date, displayedComponents: .date)
+                        
+                        Button("Set", action: {
+                            AppDelegate.braze?.user.set(dateOfBirth: date)
+                            UINotificationFeedbackGenerator().notificationOccurred(.success)
+                        })
+                        
+                    }
+                }
+                
+                Section("Custom Attributes") {
+                    HStack {
+                        TextField("Key", text: $customAttributeKey)
+                        TextField("Value", text: $customAttributeValue)
+                        Button("Set", action: {
+                            AppDelegate.braze?.user.setCustomAttribute(key: customAttributeKey, value: customAttributeValue)
+                            UINotificationFeedbackGenerator().notificationOccurred(.success)
+                            customAttributeKey = ""
+                            customAttributeValue = ""
+                        })
+                    }
+                }
+                
+                Section("Custom Events") {
+                    HStack {
+                        TextField("Event Name", text: $customEventName)
+                        
+                        Button("Log", action: logCustomEvent)
+                    }
+                    
+                    HStack {
+                        TextField("Key", text: $customEventPropKey)
+                        TextField("Value", text: $customEventPropValue)
+                    }
+                }
+                
+                Section("Purchase Events") {
+                    HStack {
+                        TextField("Product ID", text: $productID)
+                        
+                        Button("Log", action: logPurchaseEvent)
+                    }
+                    TextField("Price", value: $price, format: .currency(code: Locale.current.currency?.identifier ?? currency))
+                        .keyboardType(.decimalPad)
+                    Stepper("\(quantity)", value: $quantity, in: 1...25)
+                    HStack {
+                        TextField("Key", text: $purchaseEventPropKey)
+                        TextField("Value", text: $purchaseEventPropValue)
+                    }
+                }
+                
+            }
+            .navigationTitle("Torchie App")
         }
-        .padding()
     }
+    
+    //FUNCTIONS
+    func logCustomEvent() {
+        if customEventPropKey == "" && customEventPropValue == "" {
+            AppDelegate.braze?.logCustomEvent(name: customEventName)
+            UINotificationFeedbackGenerator().notificationOccurred(.success)
+            customEventName = ""
+        } else {
+            AppDelegate.braze?.logCustomEvent(name: customEventName, properties: [customEventPropKey: customEventPropValue])
+            UINotificationFeedbackGenerator().notificationOccurred(.success)
+            customEventName = ""
+            customEventPropKey = ""
+            customEventPropValue = ""
+        }
+        showAlert = true
+    }
+    
+    func logPurchaseEvent() {
+        if purchaseEventPropKey == "" && purchaseEventPropValue == "" {
+            AppDelegate.braze?.logPurchase (productId: productID, currency: currency, price: price, quantity: quantity)
+            UINotificationFeedbackGenerator().notificationOccurred(.success)
+            productID = ""
+            price = 0.0
+            quantity = 1
+        } else {
+            AppDelegate.braze?.logPurchase (productId: productID, currency: currency, price: price, quantity: quantity, properties: [purchaseEventPropKey : purchaseEventPropValue])
+            UINotificationFeedbackGenerator().notificationOccurred(.success)
+            productID = ""
+            price = 0.0
+            quantity = 1
+            purchaseEventPropKey = ""
+            purchaseEventPropValue = ""
+        }
+        showAlert = true
+    }
+    
+
+    
 }
 
 #Preview {
--- Torchie/TorchieApp.swift
+++ Torchie/TorchieApp.swift
@@ -5,12 +5,54 @@
 //  Created by Kevin Rodriguez on 1/30/24.
 //
 
+import BrazeKit
 import SwiftUI
 
+//In summary, this code sets up and configures the Braze SDK in the AppDelegate class, ensuring that the Braze instance is stored for later use throughout the app's lifecycle.
+
+
+//Defines a class named AppDelegate that inherits from NSObject and conforms to the UIApplicationDelegate protocol. The AppDelegate is designated as the delegate to manage the application's lifecycle events - handling tasks associated with launching, backgrounding, foregrounding, and termination.
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    //Declares a static property named braze of type Braze? (optional Braze instance). This property is used to store a reference to the Braze instance.
+    static var braze: Braze? = nil
+    
+    // This method is a part of the UIApplicationDelegate protocol and is called when the app is launching. The purpose of this method is to perform any necessary setup tasks before the app is considered fully launched and becomes active.
+    //This function takes two parameters:
+    //application: An instance of UIApplication. This parameter represents the application object itself.
+    //launchOptions: A dictionary of options that may contain information about the reason the app was launched.It's an optional parameter (? indicates optional) and is assigned a default value of nil if not provided.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        //Creates an instance of the Braze SDK (braze) using the provided configuration.
+        let configuration = Braze.Configuration(
+            apiKey: "73c9f921-d20f-4e7d-9253-e56c923ae69a",
+            endpoint: "sondheim.braze.com"
+        )
+        let braze = Braze(configuration: configuration)
+        
+        //Stores the Braze instance in the static property AppDelegate.braze for future reference.
+        AppDelegate.braze = braze
+       
+        //Returns true to indicate that the app has finished launching successfully.
+        return true
+    }
+}
+
+//The @main attribute is an entry point for the application, indicating that this structure is the application itself.
 @main
+
+//This declares a structure named TorchieApp that conforms to the App protocol. In SwiftUI, the App protocol is used to define the structure of an application.
 struct TorchieApp: App {
+    
+    //This property uses the @UIApplicationDelegateAdaptor attribute to associate an instance of the AppDelegate class with the TorchieApp. It specifies that AppDelegate is the delegate for managing the application's lifecycle events.
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
+        
+    //The WindowGroup scene represents the main window of the application.
         WindowGroup {
+            
+        //Within the WindowGroup, the ContentView() is set as the root view of the main window. ContentView is presumably the main user interface of the app.
             ContentView()
         }
     }
